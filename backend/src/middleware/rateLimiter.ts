8d42378ba344054c00cdae23cb47a02d6edf47d8
Shreya Chopra <chopra.shreya03@gmail.com>
2026-09-04 14:09:11 +0530

    update: add ipkeyGenerator for normalization of ip address

--- backend/src/middleware/rateLimiter.ts
+++ backend/src/middleware/rateLimiter.ts
@@ -1,11 +1,12 @@
-import rateLimit from "express-rate-limit";
+import rateLimit, { ipKeyGenerator } from "express-rate-limit";
 import crypto from "crypto";
 
 export const analyzeRateLimiter = rateLimit ({
     windowMs: 60*60*100,
     max: 10,
     keyGenerator: (req) => {
-        return crypto.createHash("sha256").update(req.ip ?? "unknown").digest("hex");
+        const normalizedIp = ipKeyGenerator(req.ip ?? "unknown");
+        return crypto.createHash("sha256").update(normalizedIp).digest("hex");
     },
     handler: (req, res) => {
         res.status(429).json("Limit exceeded, try after sometime.");
