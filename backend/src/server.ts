735d6c8150ce53a9ee37d10bd388e06f51edd883
Shreya Chopra <chopra.shreya03@gmail.com>
2026-09-04 14:08:01 +0530

    feat: connect the database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,14 @@
+import mongoose from "mongoose";
 import { env } from "./config/env.js";
 import { app } from "./app.js";
 
-app.listen(env.PORT, () => {
-    console.log(`Server is listening on ${env.PORT}`)
-});
-
+mongoose.connect(env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+        app.listen(env.PORT, () => {
+            console.log(`Server is listening on ${env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("MongoDB connection failed:", error);
+    });
