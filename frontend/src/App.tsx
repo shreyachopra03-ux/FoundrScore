65c4356c13e7af042c66a13d64edfbe68b7d800d
Shreya Chopra <chopra.shreya03@gmail.com>
2026-09-08 22:10:27 +0530

    refactor: minor code cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,15 @@ import IdeaInputForm from "../src/components/IdeaInputForm";
 import { type AnalyzeResponse } from "./types/index";
 
 function App() {
-  const [score, setScore] = useState<AnalyzeResponse | null>(null);
+  const [result, setResult] = useState<AnalyzeResponse | null>(null);
 
   return (
     <>
       <div className="heading font-extrabold m-0">
         FoundrScore Project
       </div>
-      <IdeaInputForm onResult={setScore} />
-      {score && <p>{JSON.stringify(score)}</p>}
+      <IdeaInputForm onResult={setResult} />
+      {result && <p>{JSON.stringify(result)}</p>}
     </>
   );
 }
